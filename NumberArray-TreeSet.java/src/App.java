b6baebf9a4d9332d64a71149900283f670592dc2
Seun <olagunjuoluwaseun1@gmail.com>
2022-03-09 01:18:46 +0100

    Working with TreeSet in Java

---
+++ NumberArray-TreeSet.java/src/App.java
@@ -0,0 +1,20 @@
+import java.util.TreeSet;
+
+
+
+public class App {
+
+  public static void main(String args[]) {
+
+    int[] numbers = { 1, 4, 5, 2, 34, 66, 5, 4, 33, 45, 6, 8, 56, 76, 78, 98, 34, 37, 12, 12, 23, 43, 54, 56 };
+
+    TreeSet<Integer> set = new TreeSet<>();
+
+    for(int num : numbers) {
+      set.add(num);
+    }
+
+    System.out.println("Smallest element " + set.first());
+    System.out.println("Largest element " + set.last());
+    }
+}
